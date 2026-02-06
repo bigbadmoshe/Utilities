6d4fd9741107e7c784d0d0f1b0b94453cdebd461
Dmitry Nefedov <nefedovds@ya.ru>
2026-02-06 19:55:33 +0300

    Update yt-dlp.ps1

--- Download/yt-dlp.ps1
+++ Download/yt-dlp.ps1
@@ -16,6 +16,8 @@
 	.LINKS
 	https://github.com/yt-dlp/yt-dlp
 	https://github.com/BtbN/FFmpeg-Builds
+	https://github.com/denoland/deno
+
 #>
 function yt-dlp
 {
@@ -35,19 +37,20 @@ function yt-dlp
 		$Script:ProgressPreference = "SilentlyContinue"
 	}
 
+	# Get the latest yt-dl URL
 	# https://github.com/yt-dlp/yt-dlp
-	# Get the latest youtube-dl build tag
 	$Parameters = @{
 		Uri              = "https://api.github.com/repos/yt-dlp/yt-dlp/releases/latest"
 		UseBasicParsing  = $true
+		Verbose          = $true
 	}
-	$LatestytdlplRelease = (Invoke-RestMethod @Parameters).tag_name
+	$LatestytdlpURL = ((Invoke-RestMethod @Parameters).assets | Where-Object -FilterScript {$_.name -eq "yt-dlp.exe"}).browser_download_url
 
 	$DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
 	if (-not (Test-Path -Path "$DownloadsFolder\yt-dlp.exe"))
 	{
 		$Parameters = @{
-			Uri              = "https://github.com/yt-dlp/yt-dlp/releases/download/$LatestytdlplRelease/yt-dlp.exe"
+			Uri              = $LatestytdlpURL
 			OutFile          = "$DownloadsFolder\yt-dlp.exe"
 			UseBasicParsing  = $true
 			Verbose          = $true
@@ -56,12 +59,13 @@ function yt-dlp
 	}
 
 	# Get the latest FFmpeg URL
- 	# https://github.com/BtbN/FFmpeg-Builds
+	# https://github.com/BtbN/FFmpeg-Builds
 	# "ffmpeg-*-win64-lgpl-[0-9].[0-9].zip"
 	# gpl includes all dependencies, even those that require full GPL instead of just LGPL
 	$Parameters = @{
 		Uri              = "https://api.github.com/repos/BtbN/FFmpeg-Builds/releases/latest"
 		UseBasicParsing  = $true
+		Verbose          = $true
 	}
 	$LatestFFmpegURL = ((Invoke-RestMethod @Parameters).assets | Where-Object -FilterScript {$_.name -eq "ffmpeg-master-latest-win64-lgpl.zip"}).browser_download_url
 
@@ -86,13 +90,43 @@ function yt-dlp
 		Remove-Item -Path "$DownloadsFolder\FFmpeg.zip" -Force
 	}
 
+	# Get the latest deno URL
+	# https://github.com/yt-dlp/yt-dlp/wiki/EJS
+	# https://github.com/denoland/deno
+	$Parameters = @{
+		Uri              = "https://api.github.com/repos/denoland/deno/releases/latest"
+		UseBasicParsing  = $true
+		Verbose          = $true
+	}
+	$LatestdenoURL = ((Invoke-RestMethod @Parameters).assets | Where-Object -FilterScript {$_.name -eq "deno-x86_64-pc-windows-msvc.zip"}).browser_download_url
+
+	if (-not (Test-Path -Path "$DownloadsFolder\deno.exe"))
+	{
+		$Parameters = @{
+			Uri              = $LatestdenoURL
+			OutFile          = "$DownloadsFolder\deno.zip"
+			UseBasicParsing  = $true
+			Verbose          = $true
+		}
+		Invoke-WebRequest @Parameters
+
+		$Parameters = @{
+			Path            = "$DownloadsFolder\deno.zip"
+			DestinationPath = "$DownloadsFolder"
+			Force           = $true
+		}
+		Expand-Archive @Parameters
+
+		Remove-Item -Path "$DownloadsFolder\deno.zip" -Force
+	}
+
+
 	$Title = "%(title)s.mp4"
 	$n = 1
 
 	foreach ($URL in $URLs)
 	{
 		# Getting URL's IDs
-		# https://www.reddit.com/r/youtubedl/comments/fzv58p/comment/fn6hass/?context=3
 		& "$DownloadsFolder\yt-dlp.exe" --list-formats $URL
 		$VideoID = Read-Host -Prompt "`nType prefered video ID"
 		$AudioID = Read-Host -Prompt "`nType prefered audio ID"
@@ -100,7 +134,7 @@ function yt-dlp
 		# 1. FileName.mp4
 		$FileName = "{0}. {1}" -f $n++, $Title
 
-		Start-Process -FilePath "$DownloadsFolder\yt-dlp.exe" -ArgumentList "--output `"$DownloadsFolder\$FileName`" --format `"$($VideoID)+$($AudioID)`" $URL"
+		Start-Process -FilePath "$DownloadsFolder\yt-dlp.exe" -ArgumentList "--js-runtimes deno:`"$DownloadsFolder\deno.exe`" --output `"$DownloadsFolder\$FileName`" --format `"$($VideoID)+$($AudioID)`" $URL"
 	}
 }
-yt-dlp -URLs @("https://www.youtube.com/watch?v=DqTEVmed0Bc")
+yt-dlp -URLs @("https://www.youtube.com/watch?v=NxY5pItZbS4")
