a9f112d6c913d79e110839e5d7a10667948ab96b
Dmitry Nefedov <nefedovds@ya.ru>
2026-02-15 23:06:34 +0300

    Update Selenium.ps1

--- Selenium.ps1
+++ Selenium.ps1
@@ -4,24 +4,14 @@
 # https://developer.microsoft.com/microsoft-edge/tools/webdriver/
 
 Import-Module "D:\Desktop\lib\net8.0\WebDriver.dll"
-Import-Module "D:\Desktop\lib\net8.0\WebDriver.Support.dll"
 
-$edgeOptions = New-Object OpenQA.Selenium.Edge.EdgeOptions
+$Options = New-Object OpenQA.Selenium.Edge.EdgeOptions
 
-$edgeOptions.AddArgument("--headless=new")
-$edgeOptions.AddArgument("--ignore-certificate-errors")
-$edgeOptions.AddArgument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0")
-$edgeOptions.AddArgument("--window-size=1920,1080")
-$edgeOptions.AddArgument("--disable-blink-features=AutomationControlled")
-$edgeOptions.AddArgument("--disable-features=IsolateOrigins,site-per-process")
-$edgeOptions.AddArgument("--disable-site-isolation-trials")
+$options = New-Object OpenQA.Selenium.Edge.EdgeOptions
+$options.AddArgument("--headless=new")
+$options.AddArgument("--window-size=1280,720")
+$options.AddArgument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0")
 
-$edgeOptions.AcceptInsecureCertificates = $true
-$edgeOptions.AddUserProfilePreference("download.default_directory", "D:\Desktop\lib")
-
-$driver = New-Object OpenQA.Selenium.Edge.EdgeDriver("D:\Desktop\lib", $edgeOptions)
-
-$driver.ExecuteScript("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-$driver.ExecuteScript("return navigator.language")
-
+$driver = New-Object OpenQA.Selenium.Edge.EdgeDriver("D:\Desktop\lib\msedgedriver.exe", $Options)
 $driver.Navigate().GoToUrl("URL")
+#$driver.Quit()
